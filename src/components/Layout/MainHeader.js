15219e2278f417df1e9f1eab05ce8cd0913f4c56
jydnbach <jaydenkim95@gmail.com>
2024-06-16 15:14:36 +0900

    uislice show cart

--- src/components/Layout/MainHeader.js
+++ src/components/Layout/MainHeader.js
@@ -1,23 +1,14 @@
 import CartButton from '../Cart/CartButton';
 import classes from './MainHeader.module.css';
-import { uiActions } from '../store/ui-slice';
-import { useDispatch, useSelector } from 'react-redux';
 
 const MainHeader = (props) => {
-  const dispatch = useDispatch();
-  const showCart = useSelector((state) => state.ui.cartIsVisible);
-
-  const showCartHandler = () => {
-    dispatch(uiActions.toggle());
-  };
-
   return (
     <header className={classes.header}>
       <h1>ReduxCart</h1>
       <nav>
         <ul>
           <li>
-            <CartButton onClick={showCartHandler} />
+            <CartButton />
           </li>
         </ul>
       </nav>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-
 import uiSlice from './ui-slice';
 
 const store = configureStore({
