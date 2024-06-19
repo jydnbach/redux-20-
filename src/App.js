0026fbfb4a2ef74612e21517d7475ad9256e24d1
jydnbach <jaydenkim95@gmail.com>
2024-06-19 13:46:26 +0900

    sending cart data to backend

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -7,6 +8,14 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.carts);
+
+  useEffect(() => {
+    fetch('https://redux-73e55-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
